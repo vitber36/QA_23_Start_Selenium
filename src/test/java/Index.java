4c5ad1e7c56fc28479fa801de4f16fc7a6424fd3
vitber36 <vitber36@gmail.com>
2023-12-18 21:00:27 +0200

    tag name

---
+++ src/test/java/Index.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Index {
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd=new ChromeDriver();
+        wd.get("file:///C:/Users/User/Downloads/21.index%20(1).html");
+    }
+
+    @Test
+    public void cssLocators(){
+        //by tag name
+        WebElement element=wd.findElement(By.tagName("button"));
+        List<WebElement> list=wd.findElements(By.tagName("a"));
+    }
+
+    @AfterClass
+    public void 
+}
