c23f17fbbe04ca6c8f99d82e6b476823e60aea25
vitber36 <vitber36@gmail.com>
2023-12-20 12:50:42 +0200

    Update Index.java

--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -21,8 +21,24 @@ public class Index {
     @Test
     public void cssLocators(){
         //by tag name
-        WebElement element=wd.findElement(By.tagName("button"));
-        List<WebElement> list=wd.findElements(By.tagName("a"));
+        WebElement element1=wd.findElement(By.tagName("button"));
+        WebElement element2=wd.findElement(By.tagName("ul"));
+        WebElement element3=wd.findElement(By.tagName("table"));
+        WebElement element4=wd.findElement(By.tagName("tbody"));
+        WebElement element5=wd.findElement(By.tagName("p"));
+        WebElement element6=wd.findElement(By.tagName("script"));
+        WebElement element7=wd.findElement(By.tagName("body"));
+
+        List<WebElement> list1=wd.findElements(By.tagName("a"));
+        List<WebElement> list2=wd.findElements(By.tagName("li"));
+        List<WebElement> list3=wd.findElements(By.tagName("div"));
+        List<WebElement> list4=wd.findElements(By.tagName("hr"));
+        List<WebElement> list5=wd.findElements(By.tagName("input"));
+        List<WebElement> list6=wd.findElements(By.tagName("tr"));
+        List<WebElement> list7=wd.findElements(By.tagName("th"));
+        List<WebElement> list8=wd.findElements(By.tagName("td"));
+
+
     }
 
     @AfterClass
